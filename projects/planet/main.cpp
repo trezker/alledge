1c985cccbb5e16e27caca22470d42f54eec5ebc0
Anders Andersson <trezker@gmail.com>
2021-11-25 21:45:30 +0100

    Sphere from cube

--- projects/planet/main.cpp
+++ projects/planet/main.cpp
@@ -4,6 +4,7 @@
 #include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_opengl.h>
 #include <iostream>
+#include <cmath>
 
 #include "../../alledge/View.h"
 #include "../../alledge/Scenenode.h"
@@ -80,7 +81,7 @@ bool Init()
 	}
 	*/
 
-	int n = 100;
+	int n = 10;
 	float ns  = 2.0f/n;
 	int i = 0;
 
@@ -88,20 +89,19 @@ bool Init()
 	Indexes indices;
 	for(int x = 0; x<n; ++x) {
 		for(int y = 0; y < n; ++y) {
-			vertices.push_back(Vector3(x*ns-1,    y*ns-1,    1));
-			vertices.push_back(Vector3(x*ns+ns-1, y*ns-1,    1));
-			vertices.push_back(Vector3(x*ns-1,    y*ns+ns-1, 1));
-			vertices.push_back(Vector3(x*ns+ns-1, y*ns+ns-1, 1));
-			indices.insert(indices.end(), {i, i+2, i+1, i+1, i+2, i+3});
-			i+=4;
-
 			vertices.push_back(Vector3(x*ns-1,    y*ns-1,    -1));
 			vertices.push_back(Vector3(x*ns+ns-1, y*ns-1,    -1));
 			vertices.push_back(Vector3(x*ns-1,    y*ns+ns-1, -1));
 			vertices.push_back(Vector3(x*ns+ns-1, y*ns+ns-1, -1));
+			indices.insert(indices.end(), {i, i+2, i+1, i+1, i+2, i+3});
+			i+=4;
+
+			vertices.push_back(Vector3(x*ns-1,    y*ns-1,    1));
+			vertices.push_back(Vector3(x*ns+ns-1, y*ns-1,    1));
+			vertices.push_back(Vector3(x*ns-1,    y*ns+ns-1, 1));
+			vertices.push_back(Vector3(x*ns+ns-1, y*ns+ns-1, 1));
 			indices.insert(indices.end(), {i, i+1, i+2, i+1, i+3, i+2});
 			i+=4;
-			
 		}
 		
 		for(int z = 0; z < n; ++z) {
@@ -122,21 +122,32 @@ bool Init()
 	}
 	for(int y = 0; y < n; ++y) {
 		for(int z = 0; z < n; ++z) {
-			vertices.push_back(Vector3(1, y*ns-1,    z*ns-1));
-			vertices.push_back(Vector3(1, y*ns+ns-1, z*ns-1));
-			vertices.push_back(Vector3(1, y*ns-1,    z*ns+ns-1));
-			vertices.push_back(Vector3(1, y*ns+ns-1, z*ns+ns-1));
-			indices.insert(indices.end(), {i, i+2, i+1, i+1, i+2, i+3});
-			i+=4;
-
 			vertices.push_back(Vector3(-1, y*ns-1,    z*ns-1));
 			vertices.push_back(Vector3(-1, y*ns+ns-1, z*ns-1));
 			vertices.push_back(Vector3(-1, y*ns-1,    z*ns+ns-1));
 			vertices.push_back(Vector3(-1, y*ns+ns-1, z*ns+ns-1));
+			indices.insert(indices.end(), {i, i+2, i+1, i+1, i+2, i+3});
+			i+=4;
+
+			vertices.push_back(Vector3(1, y*ns-1,    z*ns-1));
+			vertices.push_back(Vector3(1, y*ns+ns-1, z*ns-1));
+			vertices.push_back(Vector3(1, y*ns-1,    z*ns+ns-1));
+			vertices.push_back(Vector3(1, y*ns+ns-1, z*ns+ns-1));
 			indices.insert(indices.end(), {i, i+1, i+2, i+1, i+3, i+2});
 			i+=4;
 		}
 	}
+
+	for(int i = 0; i<vertices.size(); ++i) {
+		float x2 = vertices[i].x*vertices[i].x;
+		float y2 = vertices[i].y*vertices[i].y;
+		float z2 = vertices[i].z*vertices[i].z;
+		float x = vertices[i].x * sqrt(1 - (y2 + z2) / 2 + (y2 * z2) / 3);
+		float y = vertices[i].y * sqrt(1 - (z2 + x2) / 2 + (z2 * x2) / 3);
+		float z = vertices[i].z * sqrt(1 - (x2 + y2) / 2 + (x2 * y2) / 3);
+		vertices[i] = Vector3(x, y, z);
+	}
+	
 /*
 	vertices.push_back(Vector3(-1, 1, 1));
 	vertices.push_back(Vector3( 1, 1, 1));
