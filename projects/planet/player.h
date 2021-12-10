89e16fa3bb5d97821f4c316e4ba3d7e3631e8c4a
Anders Andersson <trezker@gmail.com>
2021-12-10 18:25:50 +0100

    Refactor

--- projects/planet/player.h
+++ projects/planet/player.h
@@ -5,30 +5,5 @@ private:
 	shared_ptr<Static_model_node> model_node;
 	shared_ptr<Transformnode> transform;
 public:
-	Player(shared_ptr<Scenenode> p) {
-		parent = p;
-		transform = new Transformnode;
-		parent->Attach_node(transform);
-
-		Vectors pv;
-		Indexes pi;
-
-		pv.push_back(Vector3(-0.5, 0, 0));
-		pv.push_back(Vector3(0.5, 0, 0));
-		pv.push_back(Vector3(0, 1, 0));
-		pv.push_back(Vector3(0, 0, 0.5));
-
-		pi.insert(pi.end(), {0, 2, 3, 1, 3, 2});
-
-		model = new Static_model;
-		model->Set_model_data(pv, pi);
-		float color[4] = {1, 0, 1, 1};
-		model->Set_color(color);
-
-		model_node = new Static_model_node;
-		model_node->Set_model(model);
-		transform->Attach_node(model_node);
-		transform->Set_position(Vector3(0, 0, 1));
-		transform->Set_scale(Vector3(0.1,0.1,0.1));
-	}
+	Player(shared_ptr<Scenenode> p);
 };
